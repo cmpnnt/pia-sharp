6ffb32723b153daa49fec5065392c3daca9562ef
cmpnnt <ooe@cmpnnt.dev>
2024-04-21 18:30:22 -0400

    test: add `SwitchToFastest` tests

--- Cmpnnt.Pia.Ctl/Utilities.cs
+++ Cmpnnt.Pia.Ctl/Utilities.cs
@@ -4,9 +4,19 @@ using Cmpnnt.Pia.Ctl.Lib.Results;
 
 namespace Cmpnnt.Pia.Ctl;
 
-public class Utilities(PiaCtl piaCtl)
+public class Utilities
 {
-    private readonly PiaCtl _piaCtl = piaCtl;
+    private readonly PiaCtl _piaCtl;
+
+    public Utilities(PiaCtl piaCtl)
+    {
+        _piaCtl = piaCtl;
+    }
+    
+    public Utilities()
+    {
+        _piaCtl = new PiaCtl();
+    }
 
     /// <summary>
     /// 
@@ -15,7 +25,7 @@ public class Utilities(PiaCtl piaCtl)
     /// <returns></returns>
     public async Task<PiaResults> SwitchToFastest(MultiRegion regionPrefix)
     {
-        PiaResults regions = await piaCtl.GetRegions();
+        PiaResults regions = await _piaCtl.GetRegions();
         
         if (regions.Status != Status.Completed)
         {
@@ -23,24 +33,26 @@ public class Utilities(PiaCtl piaCtl)
             return regions;
         }
 
+        // iterate through the list of regions until we find the first one that matches our country
+        // the list of regions is already sorted by latency, so the first is the fastest
         foreach(string r in regions.StandardOutputResults)
         {
             PiaResults results = new();
             
-            var currentRegion = r.Trim().Substring(0, 2);
+            var currentRegion = r.Trim().Split("-")[0];
             
             if (!currentRegion.Equals(regionPrefix.ToStringF(), StringComparison.CurrentCultureIgnoreCase)) continue;
             
             string newRegion = r.Trim();
             results.Status = Status.Completed;
             results.StandardOutputResults = [newRegion];
-            await piaCtl.SetRegion(newRegion);
+            await _piaCtl.SetRegion(newRegion);
 
             return results;
         }
 
         var errorMessage = "Unable to switch to a new region.";
-        return new PiaResults{Status = Status.Error, StandardOutputResults = [errorMessage] };
+        return new PiaResults{Status = Status.Error, StandardErrorResults = [errorMessage] };
 
     }
 }
